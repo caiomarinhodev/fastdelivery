ab6967d562e59d5446fb7363f7c2cf5fa11f8b88
Caio Fábio Marinho Barbosa <caio.barbosa@virtus.ufcg.edu.br>
2018-12-18 12:29:53 -0300

    fixz urls

--- delivery/urls.py
+++ delivery/urls.py
@@ -1,10 +1,8 @@
 """urls.py: Urls definidas."""
 from django.conf.urls import url, include
-from django.urls import reverse
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from rest_framework import routers
-
 
 from api import views as views_api
 from api.views import UserViewSet, BairroViewSet, ConfigurationViewSet, ItemPedidoViewSet, \
